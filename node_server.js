18397d7f78ec75e5b79c5868d8dcd2c61b321b7a
Alberto <albertoue93@gmail.com>
2020-08-31 02:32:26 -0600

    solucion caso practico #3

--- node_server.js
+++ node_server.js
@@ -1,16 +1,17 @@
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
 
-http.createServer(function (req, res) {
+http.createServer(function(req, res) {
     var filePath = '.' + req.url;
 
     if (filePath == './') {
-        filePath = './index3.html';
+        filePath = './index2.html';
     }
-  
+
     fs.readFile(filePath, function(err, data) {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      return res.end(data, 'UTF-8');
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        return res.end(data, 'UTF-8');
     });
 }).listen(8080);
 console.log('Server is running');
