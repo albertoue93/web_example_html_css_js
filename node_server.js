aac8fc41339907c3978ee04aebc64e0d44c6215c
Alberto <albertoue93@gmail.com>
2020-09-04 20:51:36 -0600

    solucion caso practico dmgamboav#3

--- node_server.js
+++ node_server.js
@@ -9,8 +9,18 @@ http.createServer(function(req, res) {
         filePath = './index2.html';
     }
 
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var contentType = 'text/html';
+    var mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css'
+    };
+
+    contentType = mimeTypes[extname] || 'application/octet-stream';
+
     fs.readFile(filePath, function(err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.writeHead(200, { 'Content-Type': contentType });
         return res.end(data, 'UTF-8');
     });
 }).listen(8080);
